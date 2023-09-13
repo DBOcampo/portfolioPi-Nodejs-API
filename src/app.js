c790fd12568d227922186d423b750ddec1a8798c
Lemmingg <96708179+Lemmingg@users.noreply.github.com>
2023-09-13 06:24:02 -0300

    Changed nodemon script name to dev on package.json

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import express from "express";
+
+import routes from "./routes/routes.js";
+import eduList from "./routes/EduList.routes.js";
+import expList from "./routes/ExpList.routes.js";
+import proyects from "./routes/Proyects.routes.js";
+import infocard from "./routes/InfoCard.routes.js";
+import mainimages from "./routes/MainImages.routes.js";
+import skilldi from "./routes/SkillDi.routes.js"
+import skillhs from "./routes/SkillHs.routes.js"
+import maininfo from "./routes/MainInfo.routes.js"
+import signin from "./routes/SignIn.routes.js"
+import signup from "./routes/SignUp.routes.js"
+
+import cors from "cors";
+
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:4200", // Whitelist the specific origin
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed HTTP methods
+  credentials: true, // Enable cookies and authorization headers
+  optionsSuccessStatus: 204, // Set the response status for preflight requests
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json({ limit: "5mb" }));
+app.use(routes);
+
+app.use(eduList);
+app.use(expList);
+app.use(proyects);
+app.use(infocard);
+app.use(mainimages);
+app.use(skilldi)
+app.use(skillhs)
+app.use(maininfo)
+app.use(signin)
+app.use(signup)
+
+app.listen(3000);
+
+console.log("App listening on port 3000");
